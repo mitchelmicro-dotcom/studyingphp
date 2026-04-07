5593d2f51a7311701e9dc6f4dd92b6149be354aa
MICHEL SANTOS ANDRADE <38162076859@cmdo.policiamilitar.sp.gov.br>
2026-04-07 10:22:43 -0300

    Realizando o primeiro commit do código PHP

---
+++ echo_print.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Curso PHP</title>
+	</head>
+
+	<body>
+		<?php 
+
+			//Comentário de uma linha
+			
+			echo 'Comando echo <br/ >';
+            # Comentário de uma linha
+            echo print ('Comanto print <hr/>'); //na essencia é uma funcao
+            
+
+			/*
+			Comentario de múltiplas linhas.
+			*/
+        ?>
+        
+    </body>
+
+</hml>
