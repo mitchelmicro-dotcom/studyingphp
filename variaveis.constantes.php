da9368d8ac4b64880587ade9611781d81c29a7fa
MICHEL SANTOS ANDRADE <38162076859@cmdo.policiamilitar.sp.gov.br>
2026-04-10 10:53:56 -0300

    Aprendendo constantes no php

---
+++ variaveis.constantes.php
@@ -0,0 +1,28 @@
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Curso PHP</title>
+	</head>
+
+	<body>
+
+        <?php
+            define('BD_URL', 'endereco_bd_dev');
+            define('BD_USUARIO', 'usuario_dev');
+            define('BD_SENHA', 'senha_dev');
+            //...lógica do sistema...// 
+
+                       
+            echo BD_URL . '<br/>';
+            echo BD_USUARIO . '<br/>';
+            echo BD_SENHA . '<br/>';
+            
+            
+        ?>
+
+
+    
+		
+    </body>
+
+</hml>
