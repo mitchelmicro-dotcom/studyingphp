d28c9c9ea9bdab093e64eda72cff67046baffb16
MICHEL SANTOS ANDRADE <38162076859@cmdo.policiamilitar.sp.gov.br>
2026-04-14 10:01:49 -0300

    Realizando teste

---
+++ if_else.php
@@ -0,0 +1,32 @@
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Curso PHP IF & ELSE</title>
+	</head>
+
+	<body>
+
+        <?php
+           if(8 < 8 ){
+
+               echo '8 é menor que 8';
+
+           }else if(8 == 8){
+
+               echo '8 e 8 são iguais';
+
+           }else{
+
+               echo '8 diferente de 8';
+
+           } 
+            
+            
+        ?>
+
+
+    
+		
+    </body>
+
+</hml>
