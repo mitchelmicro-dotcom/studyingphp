d43b561dd75c9721a44cd9d9191691ca9d8c9cdc
MICHEL SANTOS ANDRADE <38162076859@cmdo.policiamilitar.sp.gov.br>
2026-04-22 10:21:46 -0300

    Estudando switch com php na pratica

---
+++ switch.php
@@ -0,0 +1,34 @@
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Curso PHP</title>
+	</head>
+
+	<body>
+
+        <?php
+
+				$parametro = 'abc';
+
+				switch($parametro) {
+						case 1:
+							echo 'Entrou no case 1';
+							break;
+						case 2:
+							echo 'Entrou no case 2';
+							break;	
+						case 3:
+							echo 'Entrou no case 3';
+							break;	
+						default:
+							echo 'Nenhum case correspondente';
+							break;	
+			   }     
+            
+        ?>
+
+
+		
+    </body>
+
+</hml>
