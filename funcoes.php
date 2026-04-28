ea52af6c2170c363ae9038a132d64cd3c9c07654
Michel <michelmsa89@gmail.com>
2026-04-28 13:40:30 -0300

    Estudando funções

---
+++ funcoes.php
@@ -0,0 +1,34 @@
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Curso PHP</title>
+	</head>
+
+	<body>
+
+        <?php
+            
+            //void
+            function exibirBoasVindas() {
+                echo "Bem-vindo ao curso de PHP!<br>";
+            }
+            exibirBoasVindas();
+            
+            //return
+            function calcularAreaTerreno($largura, $comprimento) {
+                $area = $largura * $comprimento;
+                return $area;
+            }
+            $resultado = calcularAreaTerreno(5, 24);
+            echo $resultado . " m²<br>";
+
+            $resultado = calcularAreaTerreno(6, 25);
+            echo $resultado . " m²<br>";
+
+            
+        ?>
+
+		
+    </body>
+
+</hml>
