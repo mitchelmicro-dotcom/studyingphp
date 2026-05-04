1f9109d6c570f157463d87fa42f038da9e86b9e8
Michel <michelmsa89@gmail.com>
2026-05-04 13:52:46 -0300

    Estudando funções matematicas

---
+++ funcoes_matematicas.php
@@ -0,0 +1,31 @@
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Curso PHP</title>
+	</head>
+
+	<body>
+
+        <?php
+
+            // $num = 7.49999; // -2  para ceil e -3 para floor 
+
+            // echo ceil($num) // arredonda para cima
+
+            // echo floor($num) // arredonda para baixo
+
+            // echo round($num) // arredonda com base na parte decimal, se for maior ou igual a 0.5 arredonda para cima, caso contrário arredonda para baixo
+
+            // echo rand(10, 20); // gera um número aleatório entre 1 e 10
+
+            // echo '<br />' . getrandmax(); // retorna o valor máximo que a função rand() pode retornar
+
+            echo sqrt (54); // retorna a raiz quadrada de um número
+            
+            
+        ?>
+    
+		
+    </body>
+
+</hml>
