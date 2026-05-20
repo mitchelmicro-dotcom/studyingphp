8972cca7f526bec3f8f33c4b4033a7c6a7c240e1
Michel <michelmsa89@gmail.com>
2026-05-20 13:32:35 -0300

    Aprendendo autenticação de usuário

--- app_help_desk/index.php
+++ app_help_desk/index.php
@@ -1,3 +1,6 @@
+
+
+
 <html>
   <head>
     <meta charset="utf-8" />
@@ -34,13 +37,20 @@
               Login
             </div>
             <div class="card-body">
-              <form action="valida_login.php" method="post">
+              <form action="valida_login.php" method="POST">
                 <div class="form-group">
                   <input name="email" type="email" class="form-control" placeholder="E-mail">
                 </div>
                 <div class="form-group">
                   <input name="senha" type="password" class="form-control" placeholder="Senha">
                 </div>
+
+                <? if(isset($_GET['login']) && $_GET['login'] == 'erro'){ ?>
+                  <div class="text-danger">
+                    Usuário ou senha inválido(s).
+                  </div>
+                <? } ?>
+
                 <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
               </form>
             </div>
--- app_help_desk/valida_login.php
+++ app_help_desk/valida_login.php
@@ -1,20 +1,49 @@
 <?php
 
-    // print_r($_GET);
+    //variavel que verfica se a auteticação foi realizada com sucesso
+
+    $usuarios_autenticados = false;
+
+    //usuarios do sistema
+
+    $usuarios_app = array(
+        array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+        array('email' => 'user@teste.com.br', 'senha' => 'abcd'),
+    );
+
+    /* echo '<pre>';
+    print_r($usuarios_app);
+    echo '</pre>'; */
+
+    foreach($usuarios_app as $user){
+
+        if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
+            $usuarios_autenticados = true;
+        } 
+
+    }
+
+    if($usuarios_autenticados){
+        echo 'Usuário autenticado';
+    } else {
+        header('Location: index.php?login=erro');
+    }
+
+    // print_r($_POST);
 
     // echo '<br />';
 
-    // echo $_GET['email'];
+    // echo $_POST['email'];
     // echo '<br />';
-    // echo $_GET['senha'];
+    // echo $_POST['senha'];
 
-    print_r($_POST);
+    /* print_r($_POST);
 
     echo '<br />';
 
     echo $_POST['email'];
     echo '<br />';
-    // echo $_POST['senha'];
+    echo $_POST['senha']; */
 
 
 ?>
